3360209b5e4ce88bac8afe23eea40bb64d71d21d
BroaderOrb <alex_houseman@hotmail.com>
2025-08-21 09:31:06 +0100

    Added a test comment

--- src/pages/Prices.tsx
+++ src/pages/Prices.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import MultiAssetPriceChart from "@/features/prices/components/MultiAssetPriceChart";
-
+// A comment
 export default function PricesPage() {
   return (
     <div className="p-4 md:p-6">
